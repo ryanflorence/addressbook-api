5a7b0bfc5f9b17cdea150fae7cc06316c5011936
Ryan Florence <rpflorence@gmail.com>
2013-11-09 22:22:07 -0700

    looser cors

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ var app = express()
 // all environments
 app.set('port', process.env.PORT || 3000)
 app.use(function(req, res, next) {
-  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Origin', req.headers.origin);
   res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
   res.header('Access-Control-Allow-Headers', 'Content-Type')
   if ('OPTIONS' == req.method) {
